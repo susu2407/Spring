7a9b026a61c941eda6681523a02669c8b8bc0413
DecemberHB <27hyobin@gmail.com>
2025-09-17 12:02:20 +0900

    add ch05

---
+++ ch05/src/main/java/kr/co/ch05/dto/User1DTO.java
@@ -0,0 +1,18 @@
+package kr.co.ch05.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User1DTO {
+
+    private String uid;
+    private String name;
+    private String birth;
+    private String age;
+}
